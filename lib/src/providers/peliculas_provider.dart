337ba0987ab968a57dfdc9c0bf7aa358469124d5
Dilan <duriostegui@asgbpm.com>
2021-04-15 17:19:28 -0500

    Implementación del HorizontalPageView con Stream y StreamBuilder

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -3,12 +3,31 @@
 import 'package:peliculas/src/models/pelicula_model.dart';
 
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:http/http.dart' as http;
 
 
 class PeliculasProvider{
 
+  String _apikey     = '22cad0f9eadbc09ce9f641e2318d1d0c';
+  String _url       = 'api.themoviedb.org';
+  String _language  = 'es-ES';  
+
+  int _popularesPage  = 0;
+  bool _cargando      = false;
+
+  List<Pelicula> _populares = [];
+
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>>  get popularesStream => _popularesStreamController.stream; 
+  
+  
+  void disposeStreams(){
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url)async{
     final resp = await http.get(url);
@@ -20,10 +39,6 @@ class PeliculasProvider{
     return peliculas.items;
   }
   
-  String _apikey     = '22cad0f9eadbc09ce9f641e2318d1d0c';
-  String _url       = 'api.themoviedb.org';
-  String _language  = 'es-ES';  
-
   Future<List<Pelicula>> getEnCines() async{
     final url = Uri.https(_url, '3/movie/now_playing',{
       'api_key' : _apikey,
@@ -35,11 +50,26 @@ class PeliculasProvider{
   }
 
   Future<List<Pelicula>> getPopulares() async{
+
+    if(_cargando) return[];
+
+    _cargando = true;
+    _popularesPage++;
+
+    
+
     final url = Uri.https(_url, '3/movie/popular',{
       'api_key' : _apikey,
       'language': _language,
+      'page'    : _popularesPage.toString(),
     });
-    return await _procesarRespuesta(url);
+    
+    final resp = await _procesarRespuesta(url);
+    _populares.addAll(resp);
+    popularesSink( _populares );
+    _cargando = false;
+    return resp;
+
   }
 
 }
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -5,27 +5,70 @@ class MovieHorizontal extends StatelessWidget {
 
   final List<Pelicula> peliculas;
 
-  MovieHorizontal({@required this.peliculas});
+  final Function siguientePagina;
+
+  MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
+
+  final _pageController = PageController(
+    initialPage: 1,
+    viewportFraction: 0.3,
+  );
+
   @override
   Widget build(BuildContext context) {
     
     final _screenSize = MediaQuery.of(context).size;
 
+    _pageController.addListener(() {
+      if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200){
+        siguientePagina();
+      }
+    });
+
     return Container(
       height: _screenSize.height * 0.28,
-      child: PageView(
+      child: PageView.builder(
         pageSnapping: false,
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3,
-        ),
-        children: _tarjetas(context),
+        controller: _pageController,
+        itemCount: peliculas.length,
+        itemBuilder: (context,i) => _tarjeta(context, peliculas[i])
+        //children: _tarjetas(context),
+      ),
+    );
+  }
+
+
+  Widget _tarjeta(BuildContext context, Pelicula pelicula){
+    final _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.only(right: 15.0),
+      child: Column(
+         children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'), 
+              image: NetworkImage(pelicula.getPosterImg()),
+              fit: BoxFit.cover,
+              height: _screenSize.height*0.22,
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Text(
+            pelicula.title,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.caption,
+          ),
+            
+        ],
       ),
     );
   }
 
   List<Widget> _tarjetas(BuildContext context){
     final _screenSize = MediaQuery.of(context).size;
+
     return peliculas.map((pelicula) {
       return Container(
         margin: EdgeInsets.only(right: 15.0),
