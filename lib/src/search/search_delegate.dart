2e07bb01e96b34dbcdbd2a2095b51fb6577bfe77
Dilan <duriostegui@asgbpm.com>
2021-04-16 13:12:03 -0500

    Uso de SearchDelegate para implementar la busqueda de peliculas

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+
+
+class DataSearch extends SearchDelegate{
+
+  String seleccion = '';
+
+  final peliculasProvider = PeliculasProvider();
+
+
+  final peliculas = [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam',
+    'Ironman',
+    'Capitan America',
+    'superman',
+    'iron man 2',
+    'iron man 3',
+    'iron man 4',
+    'iron man 5',
+
+  ];
+
+  final peliculasRecientes = [
+    'Spiderman',
+    'Capitan America'
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+      //Las acciones de nuestro AppBar
+      //throw UnimplementedError();
+      return [
+        IconButton(
+          icon: Icon(Icons.clear), 
+          onPressed: (){
+            query = '';
+          }
+        ),
+      ];
+    }
+  
+    @override
+    Widget buildLeading(BuildContext context) {
+      //Icono a la izquierda del AppBar
+      //throw UnimplementedError();
+      return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: (){
+          close(context, null);
+        }
+      );
+    }
+  
+    @override
+    Widget buildResults(BuildContext context) {
+      // Crea los resultados que vamos a mostrar
+      //throw UnimplementedError();
+      return Center(
+        child: Container(
+          height: 100.0,
+          width: 100.0,
+          color: Colors.blueAccent,
+          child: Text(seleccion),
+        ),
+      );
+    }
+  
+    @override
+    Widget buildSuggestions(BuildContext context) {
+    // Son las sugerencias que aparecen cuando la persona escribe
+    
+      if(query.isEmpty){
+        return Container();
+      }
+      
+      return FutureBuilder(
+        future: peliculasProvider.buscarPelicula(query),
+        builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+          if( snapshot.hasData ){
+            
+            final peliculas = snapshot.data;
+            
+            return ListView(
+              children: peliculas.map( (pelicula) {
+                return ListTile(
+                  leading: FadeInImage(
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    image: NetworkImage(pelicula.getPosterImg()),
+                    width: 50.0,
+                    fit: BoxFit.contain,
+                  ),
+                  title: Text(pelicula.title),
+                  subtitle: Text(pelicula.originalTitle),
+                  onTap: (){
+                    close(context, null);
+                    pelicula.uniqueId = '';
+                    Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                  },
+                );
+              }).toList()
+            );
+          }
+          else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      );
+    
+  }
+
+
+
+
+/*
+  @override
+    Widget buildSuggestions(BuildContext context) {
+    // Son las sugerencias que aparecen cuando la persona escribe
+    //throw UnimplementedError();
+    
+    final listaSugerida = ( query.isEmpty )
+                            ? peliculasRecientes
+                            : peliculas.where(
+                              (p) => p.toLowerCase().startsWith(query.toLowerCase())
+                            ).toList();
+    
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i){
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: (){
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+
+        );
+      },
+    );
+  }
+*/
+}
